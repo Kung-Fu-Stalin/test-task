55051a76431fceb2825036adbe0b274e5b1a539e
predator <sergey.predatorzzz@gmail.com>
2018-04-28 14:40:02 +0300

    First stable version 0.01

--- test_task.py
+++ test_task.py
@@ -7,11 +7,10 @@ import requests
 from zipfile import ZipFile
 
 
-
 def copy_files(source, destination=os.getcwd()):
     if sys.platform == 'win32':
         os.system('xcopy {0} {1}'.format(source, destination))
-        #shutil.copyfileobj(source, destination) TODO: Check on Win 10
+        # shutil.copyfileobj(source, destination) TODO: Check on Win 10
     else:
         shutil.copy(source, destination)
 
@@ -24,28 +23,32 @@ def create_write_file(word):
         open(file_name, 'a').close()
     if first_symbol in files_names:
         with open(file_name, 'a') as f:
-            f.write(word + '\n')
+            f.write(word + '\n') # TODO: Try to remove '\n'
 
 
 def unzip_file(local_file):
     unzip_name = '.'.join(local_file.split('.')[0:2])
     with gzip.open(local_file, 'rb') as f_in:
         with open(unzip_name, 'wb') as f_out:
-            shutil.copyfileobj(f_in, f_out) #It's STREAM!!!! not a file
+            shutil.copyfileobj(f_in, f_out)  # It's STREAM!!!! not a file
 
 
 def zip_file(cwd=os.getcwd()):
-    files_to_zip = [chr(x) + '.txt' for x in range(ord('a'), ord('z') + 1)]
+    files_to_zip = list(filter(lambda x: re.search(r'^[a-z].txt$', x), os.listdir(cwd)))
     with ZipFile('dictionary.zip', 'w') as zip:
         for file_name in files_to_zip:
             zip.write(file_name)
+    for zipped_file in files_to_zip:
+        os.remove(cwd + '/' + zipped_file)
+        # print(cwd + '/' + zipped_file)
 
 
 def download_file(url):
     local_filename = url.split('/')[-1]
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) \
                 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    my_request = get(url, stream=True, headers=headers)
+    #file_size = requests.head(url).headers['Content-Length'] TODO: Downloading progress
+    my_request = requests.get(url, stream=True, headers=headers)
     print('Request code: ', my_request.status_code)
     if my_request.status_code == 200:
         with open(local_filename, 'wb') as f:
@@ -60,7 +63,7 @@ def read_file(local_file):
     with open(local_file, 'r') as f:
         for line in f:
             if re.search(r'[a-zA-Z\s]', line[0]):
-                print(line.replace(' ', '').lower())
+                create_write_file(line.replace(' ', '').lower())
 
 
 def main(arg):
@@ -79,15 +82,13 @@ def main(arg):
         unzip_file(local_filename)
         print('Function unzip Done')
     read_file(arg)
+    zip_file()
+    print('Done')
 
 
 if __name__ == '__main__':
-    create_write_file('apple')
-    create_write_file('aspen')
     while True:
         command = str(input('Enter path to the file or URL: '))
-        # command = '10K-english-words.txt'
-        # command = 'file.txt'
         if command != 'exit':
             if command.endswith(('.txt', '.txt.gz')):
                 main(command)

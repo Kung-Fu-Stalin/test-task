31f39e31174d87284841d15317c7dc3e2f9d5731
predator <sergey.predatorzzz@gmail.com>
2018-05-01 00:43:26 +0300

    Stable version 0.06 fixed

--- test_task.py
+++ test_task.py
@@ -7,29 +7,28 @@ import requests
 from zipfile import ZipFile
 
 
-
+FILE_NAMES = [chr(x) for x in range(ord('a'), ord('z') + 1)]
 
 
 def remove_temp_files(is_archive, local_filename):
     if is_archive is True:
-        os.remove(os.getcwd() + '/' + local_filename) # TODO: WIN32 check
+        os.remove(os.getcwd() + '/' + local_filename)
 
 
-def copy_files(source, destination=os.getcwd()):
-    if sys.platform == 'win32':
-        os.system('xcopy {0} {1}'.format(source, destination))
-            # shutil.copyfileobj(source, destination) TODO: Check on Win 10
-    else:
+def copy_files(source):
+    try:
+        destination = os.getcwd()
         shutil.copy(source, destination)
+    except shutil.SameFileError:
+        print('[E] This file already exist in working directory!')
 
 
 def create_write_file(word):
-    files_names = [chr(x) for x in range(ord('a'), ord('z') + 1)]
     first_symbol = word[0]
     file_name = first_symbol + '.txt'
     if not os.path.isfile(file_name):
         open(file_name, 'a').close()
-    if first_symbol in files_names:
+    if first_symbol in FILE_NAMES:
         with open(file_name, 'a') as f:
             f.write(word)
 
@@ -42,13 +41,14 @@ def unzip_file(local_file):
     return unzip_name
 
 
-def zip_file(cwd=os.getcwd()):
+def zip_file():
+    cwd = os.getcwd()
     files_to_zip = list(filter(lambda x: re.search(r'^[a-z].txt$', x), os.listdir(cwd)))
     with ZipFile('dictionary.zip', 'w') as zip_archive:
         for file_name in files_to_zip:
             zip_archive.write(file_name)
     for zipped_file in files_to_zip:
-        os.remove(cwd + '/' + zipped_file) # TODO: WIN32 check
+        os.remove(cwd + '/' + zipped_file)
 
 
 def download_file(url):
@@ -64,9 +64,9 @@ def download_file(url):
                     if chunk:
                         f.write(chunk)
             return local_filename
-        return False
     except requests.exceptions.ConnectionError:
-        return False
+        pass
+    return False
 
 
 def read_file(local_file):
@@ -79,39 +79,38 @@ def read_file(local_file):
 def main():
     while True:
         try:
-            arg = str(input('Enter path to the file or URL: '))
-            local_filename = arg.split('/')[-1]
-            if arg == 'exit':
+            command = str(input('Enter path to the file or URL: '))
+            local_filename = command.split('/')[-1]
+            if command == 'exit':
                 sys.exit()
-            if not arg.endswith(('.txt', '.txt.gz')):
-                print('[E]',local_filename, 'is not *.txt or *.txt.gz file')
+            if not command.endswith(('.txt', '.txt.gz')):
+                print('[E]', local_filename, 'is not *.txt or *.txt.gz file')
             else:
-                isArchive = False
-                if arg.startswith('http'):
-                    downloaded_file = download_file(arg)
+
+                is_archive = False
+                if command.startswith('http'):
+                    downloaded_file = download_file(command)
                     if downloaded_file is False:
                         print('[E] Bad request')
                         continue
                     else:
                         print('File: {0} is downloaded!'.format(downloaded_file))
-                elif '/' in arg:  # TODO: Add backslash from win32
-                    if os.path.isfile(arg):
-                        copy_files(arg)
+                elif '/' in command:
+                    if os.path.isfile(command):
+                        copy_files(command)
                         print('File: copy {} created'.format(local_filename))
-                if arg.endswith('.gz'):
+                if command.endswith('.gz'):
+                    is_archive = True
                     local_filename = unzip_file(local_filename)
                     print('Function unzip Done')
-                    isArchive = True
                 read_file(local_filename)
                 zip_file()
-                remove_temp_files(isArchive, local_filename)
+                remove_temp_files(is_archive, local_filename)
                 print('Done')
         except FileNotFoundError:
             print('[E] File not found!')
         except UnicodeDecodeError:
             print('[E] Decode Error: file is not \'.txt\' or \'.txt.gz\', but have one of this file extension!')
-        except shutil.SameFileError:
-            print('[E] This file alredy exsist!')
 
 
 if __name__ == '__main__':
